cd6d0dd01956fcb6d6f7b63f64866adf46229cbe
Aquamarox <glebbabiy@gmail.com>
2025-12-30 18:42:30 +0600

    Update get_ordered_comments_by_likes.py

--- HW/project/utils /get_ordered_comments_by_likes.py
+++ HW/project/utils /get_ordered_comments_by_likes.py
@@ -1,2 +1,6 @@
-def get_ordered_comments_by_likes(comments):
-    pass
+from typing import List
+from models.comment import Comment
+
+
+def get_ordered_comments_by_likes(comments: List[Comment]) -> List[Comment]:
+    return sorted(comments, key=lambda x: x.like_count, reverse=True)
