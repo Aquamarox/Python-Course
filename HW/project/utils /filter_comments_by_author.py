43708435bca201c98defbeba79afbd9c6def200e
Aquamarox <glebbabiy@gmail.com>
2025-12-30 18:42:20 +0600

    Update filter_comments_by_author.py

--- HW/project/utils /filter_comments_by_author.py
+++ HW/project/utils /filter_comments_by_author.py
@@ -1,2 +1,7 @@
-def filter_comments_by_author(comments, author):
-    pass
+from typing import List
+from models.comment import Comment
+from models.user import User
+
+
+def filter_comments_by_author(comments: List[Comment], author: User) -> List[Comment]:
+    return [comment for comment in comments if comment.author_id == author.id]
