767fda1526d67cb65d1da2619d37c3e7794cba52
Aquamarox <glebbabiy@gmail.com>
2025-12-30 18:45:38 +0600

    Update select_top_users_by_rate.py

--- HW/project/utils /select_top_users_by_rate.py
+++ HW/project/utils /select_top_users_by_rate.py
@@ -1,2 +1,10 @@
-def select_top_users_by_rate(users, top_size):
-    pass
+from typing import List
+from models.user import User
+
+
+def select_top_users_by_rate(users: List[User], top_size: int) -> List[User]:
+    if top_size <= 0:
+        return []
+    
+    sorted_users = sorted(users, key=lambda x: x.rate, reverse=True)
+    return sorted_users[:top_size]
