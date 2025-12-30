a06784018117d861bc12ccd25b74963bf5240e83
Aquamarox <glebbabiy@gmail.com>
2025-12-30 18:42:01 +0600

    Update extract_prices.py

--- HW/project/utils /extract_prices.py
+++ HW/project/utils /extract_prices.py
@@ -1,2 +1,5 @@
-def extract_prices(products):
-    pass
+from typing import List
+from models.product import Product
+
+def extract_prices(products: List[Product]) -> List[float]:
+    return [product.get_price() for product in products]
